d243047110eaf8719eaa231b3cc7dbb203ba16b2
shibo <shibo.liu@saiwider.com>
2023-12-20 06:59:08 +0000

    device: add device KUKA module.

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /mnt/c/Code/SWD-VisionSoftware
+# Install script for directory: /mnt/c/Code/SWD-VisionSoftware-
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -42,6 +42,11 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "/usr/bin/objdump")
 endif()
 
+if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  # Include the install script for the subdirectory.
+  include("/mnt/c/Code/SWD-VisionSoftware-/build/src/cmake_install.cmake")
+endif()
+
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
@@ -50,5 +55,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/mnt/c/Code/SWD-VisionSoftware/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/mnt/c/Code/SWD-VisionSoftware-/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "kuka_eki_hw_interface.h"
 
 int main() {
     std::cout << "1" << std::endl;
+    device::EKI* eki = new device::EKI;
+
 }
 
